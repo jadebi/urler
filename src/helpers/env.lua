e2a6c461840e5579cfca42773d5a3c159db18846
Jannick Tobler <kleinermatick@gmail.com>
2026-07-05 13:37:23 +0200

    Really small consistency fixes

--- src/helpers/env.lua
+++ src/helpers/env.lua
@@ -30,19 +30,19 @@ for _, Variable in ipairs(EnvVariables) do
   local Value = os.getenv(Variable.EnvName)
 
   if not Value then
-    error(("Environment variable '%s' is not set."):format(Variable.EnvName))
+    error(string.format("Environment variable '%s' is not set.", Variable.EnvName))
   end
 
   local Validator = Validators[Variable.Type]
 
   if not Validator then
-    error(("Unknown validator '%s'."):format(Variable.Type))
+    error(string.format("Unknown validator '%s'.", Variable.Type))
   end
 
   local Normalized, Error = Validator(Value, Variable)
 
   if not Normalized then
-    error(("Invalid value for '%s': %s"):format(Variable.EnvName, Error))
+    error(string.format("Invalid value for '%s': %s", Variable.EnvName, Error))
   end
 
   Variables[Variable.VariableName] = Normalized
