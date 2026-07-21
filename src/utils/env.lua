d8f5effc948f46ffc834b4100fdc5ebc395529d7
Jannick Tobler <kleinermatick@gmail.com>
2026-07-21 14:24:54 +0200

    1. Better errors on invalid environment variable values 2. Automatically set everything to UTF-8 (no BOM)

--- src/utils/env.lua
+++ src/utils/env.lua
@@ -31,23 +31,28 @@ local EnvVariables = {
   }
 }
 
+local function errorMsg(message)
+  io.stderr:write(message .. "\n")
+  os.exit(1)
+end
+
 for _, Variable in ipairs(EnvVariables) do
   local Value = os.getenv(Variable.EnvName)
 
   if not Value then
-    error(string.format("Environment variable '%s' is not set.", Variable.EnvName))
+    errorMsg(string.format("Environment variable '%s' is not set.", Variable.EnvName))
   end
 
   local Validator = Validators[Variable.Type]
 
   if not Validator then
-    error(string.format("Unknown validator '%s'.", Variable.Type))
+    errorMsg(string.format("Unknown validator '%s'.", Variable.Type))
   end
 
   local Normalized, Error = Validator(Value, Variable)
 
   if not Normalized then
-    error(string.format("Invalid value for '%s': %s", Variable.EnvName, Error))
+    errorMsg(string.format("Invalid value for '%s': %s", Variable.EnvName, Error))
   end
 
   Variables[Variable.VariableName] = Normalized
