d70e68f3a22d1a44370c96d882a2e86312e0ea1e
Jannick Tobler <kleinermatick@gmail.com>
2026-08-14 08:55:43 +0200

    Tiny error in the formatting of the comment

--- init.sh
+++ init.sh
@@ -80,6 +80,6 @@ log "INFO" "Generating nginx config file..."
 
 log "INFO" "Init complete, starting OpenResty"
 
-#* Start main
+# * Start main
 # try with `su-exec`; if fails, just do `exec` (may happen when not using `urler` user)
 exec "$@"
